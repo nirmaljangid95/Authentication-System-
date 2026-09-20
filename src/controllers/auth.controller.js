f852953c132ad6b210598acd2f13ca1a31599229
nirmal jangid <jangidnirmal15@gmail.com>
2026-09-20 17:49:57 +0530

    impliment access Token refresh token

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -22,41 +22,99 @@ async function register(req, res) {
         password: hasPassword
     });
 
-    const token = jwt.sign({
+    const accessToken = jwt.sign({
         id: user._id
     }, config.JWT_SECRET, {
-        expiresIn: "1d"
+        expiresIn: "15m"
     }
     )
+    const refreshToken = jwt.sign({
+        id: user._id
+    }, config.JWT_SECRET, {
+        expiresIn: "7d"
+    }
+    )
+
+    res.cookie("refreshToken", refreshToken, {
+        httpOnly: true,
+        secure: true,
+        sameSite: "strict",
+        maxAge: 7 * 24 * 60 * 1000
+
+    })
+
     res.status(201).json({
         message: "user registerd successfully",
         user: {
             username: user.username,
             email: user.email
         },
-        token
+        accessToken
     })
 
 }
-async function getMe(req,res){
-     const token = req.headers.authorization?.split(" ")[1];
-     if(!token){
+async function getMe(req, res) {
+    const token = req.headers.authorization?.split(" ")[1];
+    if (!token) {
         return res.status(401).json({
-            message:"token not found"
+            message: "token not found"
         })
-     }
-     const decoded = jwt.verify(token, config.JWT_SECRET)
+    }
+    const decoded = jwt.verify(token, config.JWT_SECRET)
     //  console.log(decoded)
     const user = await UserModel.findById(decoded.id)
 
     res.status(200).json({
-        message:"user fetched successfully",
+        message: "user fetched successfully",
         user:
         {
-            username : user.username,
+            username: user.username,
             email: user.email
         }
     })
 }
 
-export  {register,getMe}
+async function refreshToken(req, res) {
+    const refreshToken = req.cookies.refreshToken;
+    if (!refreshToken) {
+        return res.status(401).json({
+            message: "refresh token not found"
+        })
+    }
+    const decoded = jwt.verify(refreshToken, config.JWT_SECRET)
+
+    const accessToken = jwt.sign({
+        id: decoded.id
+    }, config.JWT_SECRET, {
+        expiresIn: "15m"
+    })
+    
+    const newRefreshToken = jwt.sign({
+        id:decoded.id
+    }, config.JWT_SECRET,
+    {
+        expiresIn : "7d"
+    })
+
+    res.cookie("refreshToken",newRefreshToken,{
+        httpOnly:true,
+        secure:true,
+        sameSite :"strict",
+        maxAge : 7*24*60*60*1000
+    })
+    
+    // res.cookie("refreshToken", refreshToken, {
+    //     httpOnly: true,
+    //     secure: true,
+    //     sameSite: "strict",
+    //     maxAge: 7 * 24 * 60 * 1000
+
+    // })
+
+    res.status(200).json({
+        message:"Access token refreshed successfully",
+        accessToken
+    })
+}
+
+export { register, getMe, refreshToken }
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
-import {register, getMe} from "../controllers/auth.controller.js"
+import {register, getMe,refreshToken} from "../controllers/auth.controller.js"
 const authroute = Router();
 
 
 authroute.post("/register",register);
 authroute.get("/get-me",getMe);
+authroute.get("/refresh-token",refreshToken);
+
 
 export default authroute
